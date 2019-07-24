95f6f4e84602399ee5c79c9b9e762a8e52340db1
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-24 00:26:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,5 +9,6 @@ end
 introduction_with_language("Israel","Ruby")
 
 def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in Ruby."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
+introduction_with_language_optional("Israel", "Ruby")
